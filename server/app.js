d690e8bc7df5a59f0dd583585babf27b0154065b
corinamarie <corina.bernstein@gmail.com>
2015-07-29 09:52:30 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,7 @@
+//don't push node modules to git/server, it takes up a lot of space and are not needed on heroku
+// .dsstore file > anytime you navigate to anyting in finder, macOS uses this to display info
+//creating any file with "dot" at beginning will make it invisible in file structure. gitignore file will tell server which KINDS of files to ignore broadly
+//npm is a package manager, there are others such as "bower" -- is a sort of file manager that takes and deals with the files we need
+//to add bootstrap, etc just do "npm install [the thing] --save"
+//npm uninstall express --save will uninstall whatever
+// .idea folder webstorm's file to save settings etc, add to .gitignore file
